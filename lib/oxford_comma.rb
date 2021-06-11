3ab27b493a6022e2f3f2235e75f33ebb5b587d9e
Matthew <mattj20@gmail.com>
2021-06-10 18:35:41 -0700

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,24 @@
 def oxford_comma(array)
-
-end
+    new_array = ""
+    if array.size == 1
+        return array.join
+    elsif array.size == 2
+        return array.join(' and ')
+    elsif array.size == 3
+        counter = 0
+        until counter == 2
+            new_array += "#{array[counter]}, "
+            counter+=1
+        end
+        new_array += "and #{array[-1]}"
+        return new_array
+    elsif array.size > 3
+        counter = 0
+        until counter == array.size - 1
+            new_array += "#{array[counter]}, "
+            counter+=1
+        end
+        new_array += "and #{array[-1]}"
+        return new_array
+    end
+    end
